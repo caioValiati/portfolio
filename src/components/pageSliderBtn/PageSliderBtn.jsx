b50ddc9161fb907b555ca44bf6ebacc45d2e2caa
Caio Valiati <93957101+caioValiati@users.noreply.github.com>
2023-03-20 07:16:40 -0300

    route fix

--- src/components/pageSliderBtn/PageSliderBtn.jsx
+++ src/components/pageSliderBtn/PageSliderBtn.jsx
@@ -4,7 +4,7 @@ import './pageSliderBtn.css'
 
 export function PageSliderBtn({text, theme, route}) {
     return (
-        <Link to={`https://caio-geraldo.vercel.app/${route}`} className="see-more-btn"> 
+        <Link to={`https://caioGeraldo.vercel.app/${route}`} className="see-more-btn"> 
             <p className="p see-more">{text}</p>
             <img src={`media/icons/right-arrow-${theme}.png`} />
         </Link>
